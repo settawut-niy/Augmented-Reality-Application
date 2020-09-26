6fd77aff51ebe7a0827b89dfa73a2740e32c7b4c
settawut-niy <settawut.niy@gmail.com>
2020-09-26 14:09:21 +0700

    Created auto find otherSphereCharacter behavior
    - Created FindOtherSphereCharacter()

--- Assets/_Scripts/SphereCharacter.cs
+++ Assets/_Scripts/SphereCharacter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SphereCharacter : MonoBehaviour
 {
-    [SerializeField] SphereCharacter otherSphereCharacter;
+    SphereCharacter otherSphereCharacter;
 
     Renderer sphereRenderer;
     Color randomColor;
@@ -18,7 +18,35 @@ public class SphereCharacter : MonoBehaviour
 
     void Update()
     {
+        FindOtherSphereCharacter();
+
         CheckSameColor();
+    }
+
+    void FindOtherSphereCharacter()
+    {
+        if (FindObjectsOfType<SphereCharacter>().Length <= 1) { return; }
+
+        if (otherSphereCharacter != null) { return; }
+
+        for (int i = 0; i < FindObjectsOfType<SphereCharacter>().Length; i++)
+        {
+            if (otherSphereCharacter != null) { break; }
+
+            if (this == FindObjectsOfType<SphereCharacter>()[i])
+            {
+                if (i == FindObjectsOfType<SphereCharacter>().Length - 1)
+                {
+                    otherSphereCharacter = FindObjectsOfType<SphereCharacter>()[0];
+                }
+                else
+                {
+                    otherSphereCharacter = FindObjectsOfType<SphereCharacter>()[i + 1];
+                }
+            }
+        }
+
+        print("I'm " + name + " and going to" + otherSphereCharacter.name);
     }
 
     void GenerateRandomColor()
