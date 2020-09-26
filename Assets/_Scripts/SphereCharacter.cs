7b6a5db5da3cf0c359964d577dd7c9655e52f817
settawut-niy <settawut.niy@gmail.com>
2020-09-26 13:03:46 +0700

    Created SphereCharacter, random color and check same color behavior
    - Create SphereCharacter Class
    - Created GenerateRandomColor()
    - Created CheckSameColor()
    - Created relevant variables

---
+++ Assets/_Scripts/SphereCharacter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereCharacter : MonoBehaviour
+{
+    [SerializeField] SphereCharacter otherSphereCharacter;
+
+    Renderer sphereRenderer;
+    Color randomColor;
+
+    void Start()
+    {
+        sphereRenderer = GetComponent<Renderer>();
+
+        GenerateRandomColor();
+    }
+
+    void Update()
+    {
+        CheckSameColor();
+    }
+
+    void GenerateRandomColor()
+    {
+        randomColor = Random.ColorHSV();
+
+        sphereRenderer.material.color = randomColor;
+    }
+
+    void CheckSameColor()
+    {
+        if (FindObjectsOfType<SphereCharacter>().Length <= 1) { return; }
+
+        if (randomColor == otherSphereCharacter.randomColor)
+        {
+            GenerateRandomColor();
+        }
+    }
+}
