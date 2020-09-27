7e219f8f33106b810994d735de1f0cd8e1f3c012
settawut-niy <settawut.niy@gmail.com>
2020-09-27 16:35:43 +0700

    Prepared tracked image dependent situation and add image target resources
    - Created TrackedImageObserver Class
    - Add more and replace image target
    - Adjust ReferenceImageLibrary

--- Assets/_Scripts/SphereCharacter.cs
+++ Assets/_Scripts/SphereCharacter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SphereCharacter : MonoBehaviour
 {
-    //Other Charater
+    //Other Sphere Character
     SphereCharacter otherSphereCharacter;
 
     //Own attribute
---
+++ Assets/_Scripts/TrackedImageObserver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class TrackedImageObserver : MonoBehaviour
+{
+    ARTrackedImageManager trackedImageManager;
+    Vector3 m_trackedImagePosition;
+
+    public Vector3 TrackedImagePosition
+    {
+        get { return m_trackedImagePosition; }
+    }
+
+    void Awake()
+    {
+        trackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+
+    void OnEnable()
+    {
+        trackedImageManager.trackedImagesChanged += ImageChanged;
+    }
+
+    void OnDisable()
+    {
+        trackedImageManager.trackedImagesChanged -= ImageChanged;
+    }
+
+    void ImageChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (ARTrackedImage trackedImage in eventArgs.added)
+        {
+            UpdateImage(trackedImage);
+        }
+
+        foreach (ARTrackedImage trackedImage in eventArgs.updated)
+        {
+            UpdateImage(trackedImage);
+        }
+
+        foreach (ARTrackedImage trackedImage in eventArgs.removed)
+        {
+
+        }
+    }
+
+    void UpdateImage(ARTrackedImage trackedImage)
+    {
+        m_trackedImagePosition = trackedImage.transform.position;
+    }
+}
